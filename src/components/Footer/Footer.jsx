b569e0ad956425174f6778f01c53f8010ab0d733
manikantakardalli <manikanta.kardalli@gmail.com>
2026-09-01 19:08:02 +0530

    Day15_Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,35 @@
 import './Footer.css'
 
-function Footer(){
-    const year = new Date().getFullYear();
-    return(
-        <footer className='site-footer'>
-            <p>
-        &copy;{year} Manikanta. Built with React🌐
-    </p>
-        </footer>
-    )
+function Footer(){ 
+    const year = new Date().getFullYear(); 
+
+    return( 
+        <footer className='site-footer'> 
+
+            <p> 
+                &copy;{year} Manikanta. Built with React🌐 
+            </p>
+
+            <nav className="social-links" aria-label="Social links">
+                <a
+                    href="https://github.com/manikantakardalli"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>
+
+                <a
+                    href="https://www.linkedin.com/in/manikanta-kardalli-6a7165414/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    LinkedIn
+                </a>
+            </nav>
+
+        </footer> 
+    ) 
 }
-export default Footer; // We must export it so App.jsx can use it ! 
+
+export default Footer;
