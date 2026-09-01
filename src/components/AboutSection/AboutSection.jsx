d59f867feb076c7279b15bad53de254b05e7fa7e
manikantakardalli <manikanta.kardalli@gmail.com>
2026-09-01 19:17:35 +0530

    Day15_Changes

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -10,6 +10,7 @@ function AboutSection() {
         <div className='about-copy'>
           <h1>About Me</h1>
           <p>Hi! I'm Manikanta, a developer based in India building fast, accessible web experiences.</p>
+          <p>I am a passionate and motivated developer with an interest in web development and programming. I enjoy creating responsive, user-friendly websites and learning new technologies. I am continuously improving my skills through projects and practical experience, with a goal of building useful and innovative solutions.</p>
         </div>
       </div>
     </section>
